b0cc5fc0894df707809b33bd6be05487ab783ffe
Fabricio Exequiel Rios <fabriciorios.exe@gmail.com>
2024-09-10 16:33:50 -0300

    Delete listaTareas directory

--- listaTareas/src/main/java/githubfabri/listaTareas/repositorio/tareaRepositorio.java
+++
@@ -1,9 +0,0 @@
-package githubfabri.listaTareas.repositorio;
-
-import githubfabri.listaTareas.modelo.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface tareaRepositorio extends JpaRepository<Task, Long> {
-    // Puedes agregar consultas personalizadas aquí si es necesario
-}
